6e635860a24950ff6aec2d6642216c96aade34dc
michal <michal@michal.myguest.virtualbox.org>
2024-01-22 20:20:00 +0100

    new algorithm for loading textures

--- subdivide/mesh_io.py
+++ subdivide/mesh_io.py
@@ -27,13 +27,25 @@ def load_using_wave(path):
 
     for i, face in enumerate(faces):
         verticies_all[i] = numpy.concatenate((verticies[face[0]-1], normals[face[2]-1], textures_coords[face[1]-1]))
+    faces = [
+        [0, 1, 2, 3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11],
+        [12, 13, 14, 15],
+        [16, 17, 18, 19],
+        [20, 21, 22, 23]
+    ]
+    verticies = verticies_all[:, 0:3]
 
-    return (verticies_all, numpy.array(faces))
+    new_verticies, new_faces = get_proper_faces(verticies, numpy.array(faces))
+    # vertices = numpy.hstack((numpy.array(new_verticies), normals, textures_coords))
+
+    return (numpy.array(new_verticies), numpy.array(new_faces))
 
 
 
 def load_using_assimp(path):
-    with pyassimp.load(path) as scene:
+    with pyassimp.load(path, processing=pyassimp.postprocess.aiProcess_JoinIdenticalVertices) as scene:
         if len(scene.meshes) == 0:
             print("Warning: Empty mesh")
             return ([], [])
@@ -47,15 +59,14 @@ def load_using_assimp(path):
         normals = numpy.array(mesh.normals, dtype=numpy.float32)
         tex_coords = numpy.array(mesh.texturecoords[0], dtype=numpy.float32)
         tex_coords = tex_coords[:, :2]
-        print(normals)
-
+        # normals = numpy.full((positions.shape[0], 3), 0.5)
         vertices = numpy.hstack((positions, normals, tex_coords))
         faces = numpy.array(mesh.faces, dtype=numpy.uint32)
         return (vertices, faces)
 
 
 model_loaders = {
-    "obj": load_using_assimp,
+    "obj": load_using_wave,
     "stl": load_using_assimp,
     "ply": load_using_assimp,
 }
@@ -64,3 +75,29 @@ model_loaders = {
 def load_model(path):
     file_extension = os.path.splitext(path)[1][1:].lower()
     return model_loaders[file_extension](path)
+
+
+def get_proper_faces(verticies, faces):
+    new_faces = []
+    new_verticies = []
+    face_index = 0
+    for j, face in enumerate(faces):
+        new_faces.append([])
+        for i, index in enumerate(face):
+            vert_ind = position(new_verticies, verticies, index)
+            if (vert_ind == -1):
+                new_verticies.append(verticies[index])
+                new_faces[j].append(face_index)
+
+                face_index += 1
+            else:
+                new_faces[j].append(vert_ind)
+    return new_verticies, new_faces
+
+
+            
+def position(new_verticies, verticies, index):
+    for i in range(len(new_verticies)):
+        if numpy.array_equal(new_verticies[i], verticies[index]):
+           return i
+    return -1       
